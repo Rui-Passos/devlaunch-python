12174843281446a7785c88bf3d5292cfac534ef9
Rui Passos <passos.rui@gmail.com>
2026-01-01 15:38:00 +0000

    Dia 1: Python básico com input/output

---
+++ devlaunch-python/day1.py
@@ -0,0 +1,11 @@
+# Pergunta o nome do utlizador
+nome = input("Qual o teu nomer? ")
+
+# Pergunta a idade
+idade = int(input("Qual a tua idade? "))
+
+# Calcula a idade daqui a 10 anos
+idade_futura = idade + 10
+
+# Mostra o resultado
+print(f"Olá {nome}, daqui a 10 anos terás {idade_futura} anos.")
